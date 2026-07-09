7f5e538027db136c72400f6866c23161e6a6547e
sushants2711 <sushants2711@gmail.com>
2026-07-09 03:11:06 -0700

    feat: create attendance schema for tracking employee check-in and check-out data

---
+++ backend/models/attendance.model.js
@@ -0,0 +1,45 @@
+import mongoose from "mongoose";
+
+const attendanceSchema = new mongoose.Schema(
+  {
+    employee: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    date: {
+      type: Date,
+      required: true,
+    },
+    checkInTime: {
+      type: Date,
+      required: true,
+    },
+    checkOutTime: {
+      type: Date,
+    },
+    totalTimeInOffice: {
+      type: Number,
+      default: 0,
+      min: 0,
+    },
+    status: {
+      type: String,
+      enum: ["PRESENT", "ABSENT", "LEAVE", "HALF_DAY", "WEEK_OFF", "HOLIDAY"],
+      required: true,
+    },
+    leave: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Leave",
+      default: null,
+    },
+    remarks: {
+      type: String,
+      default: null,
+    },
+  },
+  { timestamps: true, minimize: true }
+);
+
+export default mongoose.models.Attendance ||
+  mongoose.model("Attendance", attendanceSchema);
