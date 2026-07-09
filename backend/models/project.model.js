a3b1c3f9af04cc42cbba5fdfe7ce2d162d15fbd0
sushants2711 <sushants2711@gmail.com>
2026-07-09 02:31:06 -0700

    feat: create project mongoose model schema

---
+++ backend/models/project.model.js
@@ -0,0 +1,47 @@
+import mongoose from "mongoose";
+
+const projectSchema = new mongoose.Schema(
+  {
+    projectName: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    teamName: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Team",
+      required: true,
+    },
+    manager: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    teamLeader: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    startDate: {
+      type: Date,
+      required: true,
+    },
+    endDate: {
+      type: Date,
+      default: null,
+    },
+    assignBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+  },
+  { timestamps: true, minimize: true }
+);
+
+export default mongoose.models.Project ||
+  mongoose.model("Project", projectSchema);
