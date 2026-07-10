f1ce45a3ae2a38699d86d0b370351ab3d450f0bb
sushants2711 <sushants2711@gmail.com>
2026-07-10 03:17:16 -0700

    feat: implement signup controller for management registration with password hashing and session cookies

--- backend/controllers/management.controller.js
+++ backend/controllers/management.controller.js
@@ -1,11 +1,53 @@
-// import managementModel from "../models/management.model.js";
+import managementModel from "../models/management.model.js";
 // import { verifyPassword } from "../utils/comparePassword.js";
-// import { hashPassword } from "../utils/hashedPassword.js";
-// import {
-//   internalServerErrorResponse,
-//   createdResponse,
-//   badRequestResponse,
-//   successResponse,
-//   notFoundResponse,
-// } from "../utils/response.handler.js";
-// import { sendCookieToUser } from "../utils/send.cookie.js";
+import { hashPassword } from "../utils/hashedPassword.js";
+import {
+  internalServerErrorResponse,
+  createdResponse,
+  badRequestResponse,
+} from "../utils/response.handler.js";
+import { sendCookieToUser } from "../utils/send.cookie.js";
+
+export const signupManagement = async (req, res) => {
+  try {
+    const { managementKey, managementPassword } = req.body;
+
+    const totalData = await managementModel.countDocuments({});
+
+    if (totalData > 5) {
+      return badRequestResponse(res, "Too many managements are registered");
+    }
+
+    const password = hashPassword(managementPassword);
+
+    const newManagement = new managementModel({
+      managementKey,
+      managementPassword: password,
+    });
+
+    const savedData = await newManagement.save().select("-managementPassword");
+
+    // send a cookie
+    try {
+      await sendCookieToUser(savedData._id, res);
+    } catch (error) {
+      return internalServerErrorResponse(
+        res,
+        "Internal Server Error",
+        error.message
+      );
+    }
+
+    return createdResponse(
+      res,
+      "Management registered successfully",
+      savedData
+    );
+  } catch (error) {
+    return internalServerErrorResponse(
+      error,
+      "Internal Server Error",
+      error.message
+    );
+  }
+};
