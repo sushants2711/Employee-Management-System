dc060901d65b955da802fe28c912e77123eb547c
sushants2711 <sushants2711@gmail.com>
2026-07-10 03:03:34 -0700

    refactor: comment out unused imports in management controller

--- backend/controllers/management.controller.js
+++ backend/controllers/management.controller.js
@@ -1,11 +1,11 @@
-import managementModel from "../models/management.model.js";
-import { verifyPassword } from "../utils/comparePassword.js";
-import { hashPassword } from "../utils/hashedPassword.js";
-import {
-  internalServerErrorResponse,
-  createdResponse,
-  badRequestResponse,
-  successResponse,
-  notFoundResponse,
-} from "../utils/response.handler.js";
-import { sendCookieToUser } from "../utils/send.cookie.js";
+// import managementModel from "../models/management.model.js";
+// import { verifyPassword } from "../utils/comparePassword.js";
+// import { hashPassword } from "../utils/hashedPassword.js";
+// import {
+//   internalServerErrorResponse,
+//   createdResponse,
+//   badRequestResponse,
+//   successResponse,
+//   notFoundResponse,
+// } from "../utils/response.handler.js";
+// import { sendCookieToUser } from "../utils/send.cookie.js";
