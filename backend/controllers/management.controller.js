1a9b969f67ff928437b64e6c4049e78976901077
sushants2711 <sushants2711@gmail.com>
2026-07-10 16:55:29 +0530

    feat: implement management login controller and refactor signup to use async password hashing

--- backend/controllers/management.controller.js
+++ backend/controllers/management.controller.js
@@ -1,14 +1,16 @@
 import managementModel from "../models/management.model.js";
-// import { verifyPassword } from "../utils/comparePassword.js";
+import { verifyPassword } from "../utils/comparePassword.js";
 import { hashPassword } from "../utils/hashedPassword.js";
 import {
   internalServerErrorResponse,
   createdResponse,
   badRequestResponse,
+  successResponse,
 } from "../utils/response.handler.js";
 import { sendCookieToUser } from "../utils/send.cookie.js";
 
-export const signupManagement = async (req, res) => {
+// management signup
+export const signupManagementController = async (req, res) => {
   try {
     const { managementKey, managementPassword } = req.body;
 
@@ -18,7 +20,7 @@ export const signupManagement = async (req, res) => {
       return badRequestResponse(res, "Too many managements are registered");
     }
 
-    const password = hashPassword(managementPassword);
+    const password = await hashPassword(managementPassword);
 
     const newManagement = new managementModel({
       managementKey,
@@ -42,6 +44,56 @@ export const signupManagement = async (req, res) => {
       res,
       "Management registered successfully",
       savedData
+    );
+  } catch (error) {
+    return internalServerErrorResponse(
+      error,
+      "Internal Server Error",
+      error.message
+    );
+  }
+};
+
+// management login
+export const loginManagement = async (req, res) => {
+  try {
+    const { managementKey, managementPassword } = req.body;
+
+    const management = await managementModel.findOne({ managementKey });
+
+    if (!management) {
+      return badRequestResponse(res, "Invalid credentials");
+    }
+
+    const isPasswordValid = await verifyPassword(
+      managementPassword,
+      management.managementPassword
+    );
+
+    if (!isPasswordValid) {
+      return badRequestResponse(res, "Invalid credentials");
+    }
+
+    // send a cookie
+    try {
+      await sendCookieToUser(management._id, res);
+    } catch (error) {
+      return internalServerErrorResponse(
+        res,
+        "Internal Server Error",
+        error.message
+      );
+    }
+
+    const managementData = {
+      _id: management._id,
+      managementKey: management.managementKey,
+    };
+
+    return successResponse(
+      res,
+      "Management logged in successfully",
+      managementData
     );
   } catch (error) {
     return internalServerErrorResponse(
