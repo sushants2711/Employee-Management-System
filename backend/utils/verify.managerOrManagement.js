6512e01fee147783511fb1d254b0313af1a4e416
sushants2711 <sushants2711@gmail.com>
2026-07-14 03:38:29 -0700

    feat: add middleware to restrict access to Manager and Management roles

---
+++ backend/utils/verify.managerOrManagement.js
@@ -0,0 +1,23 @@
+import { internalServerErrorResponse, forbiddenResponse, unauthorizedResponse } from "./response.handler.js";
+
+export const isManagerOrManagement = async (req, res, next) => {
+    try {
+        const user = req.user;
+
+        if (!user) {
+            return unauthorizedResponse(res, "Unauthorized User");
+        }
+
+        if (user.role !== "Manager" && user.role !== "Management") {
+            return forbiddenResponse(res, "Access denied");
+        }
+
+        next();
+    } catch (error) {
+        return internalServerErrorResponse(
+            res,
+            "Internal Server Error",
+            error.message
+        );
+    }
+};
