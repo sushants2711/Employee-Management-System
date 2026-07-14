85013623a4ebfa2226de0766c49d91fe02675ca3
sushants2711 <sushants2711@gmail.com>
2026-07-13 21:38:37 -0700

    feat: add update project middleware and completedAt field to project model while removing assignBy validation

--- backend/middlewares/project.middleware.js
+++ backend/middlewares/project.middleware.js
@@ -15,7 +15,6 @@ export const createProjectMiddleware = async (req, res, next) => {
       teamLeader: joi.string().hex().length(24).required(),
       startDate: joi.date().required(),
       endDate: joi.date().optional().allow(""),
-      assignBy: joi.string().hex().length(24).required(),
       members: joi.array().items(joi.string().hex().length(24)).required(),
       status: joi
         .string()
@@ -42,3 +41,72 @@ export const createProjectMiddleware = async (req, res, next) => {
 };
 
 // update project middleware
+export const updateProjectMiddleware = async (req, res, next) => {
+  try {
+    const schema = joi.object({
+      projectName: joi.string().min(5).max(500).trim().optional().allow(""),
+      description: joi.string().min(10).max(500).trim().optional().allow(""),
+      teamName: joi.string().hex().length(24).optional().allow(""),
+      manager: joi.string().hex().length(24).optional().allow(""),
+      teamLeader: joi.string().hex().length(24).optional().allow(""),
+      startDate: joi.date().optional().allow(""),
+      endDate: joi.date().optional().allow(""),
+      members: joi
+        .array()
+        .items(joi.string().hex().length(24))
+        .optional()
+        .allow(""),
+      status: joi
+        .string()
+        .valid("PLANNED", "IN_PROGRESS", "COMPLETED", "ON_HOLD")
+        .optional()
+        .allow(""),
+      isActive: joi.boolean().optional().allow(""),
+    });
+
+    const { error } = schema.validate(req.body);
+
+    if (error) {
+      return validationErrorResponse(
+        res,
+        "Error Occured at Project Validation",
+        error?.details?.[0]?.message
+      );
+    }
+
+    next();
+  } catch (error) {
+    return internalServerErrorResponse(
+      res,
+      "Internal Server Error",
+      error.message
+    );
+  }
+};
+
+// update the project completed and with date
+export const updateCompletedProjectMiddleware = async (req, res, next) => {
+  try {
+    const schema = joi.object({
+      status: joi.string().valid("COMPLETED").required(),
+    });
+
+    const { error } = schema.validate(req.body);
+
+    if (error) {
+      return validationErrorResponse(
+        res,
+        "Error Occured at Project Validation",
+        error?.details?.[0]?.message
+      );
+    }
+
+    next();
+  } catch (error) {
+    return internalServerErrorResponse(
+      res,
+      "Internal Server Error",
+      error.message
+    );
+  }
+};
